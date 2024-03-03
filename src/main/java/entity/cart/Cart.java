2ba926d7d697b6a04f762d8b4fea43a552fd05b6
tudai1101 <127816770+tudai1101@users.noreply.github.com>
2024-03-03 21:27:52 +0700

    coupling Cart.java

--- src/main/java/entity/cart/Cart.java
+++ src/main/java/entity/cart/Cart.java
@@ -59,7 +59,7 @@ public class Cart {
         }
         if (!allAvailable) throw new MediaNotAvailableException("Some media not available");
     }
-
+// Stamp coupling: truyền tham số media nhưng chỉ sử dụng id 
     public CartItem checkMediaInCart(Media media){
         for (CartItem cartItem : lstCartItem) {
             if (cartItem.getMedia().getId() == media.getId()) return cartItem;
