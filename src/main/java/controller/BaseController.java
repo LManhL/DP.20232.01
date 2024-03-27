b8162f7b4830d60cf6f01cf0df5c0fc64670aa01
tudai1101 <127816770+tudai1101@users.noreply.github.com>
2024-03-27 22:27:29 +0700

    Update BaseController.java

--- src/main/java/controller/BaseController.java
+++ src/main/java/controller/BaseController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import entity.cart.Cart;
 import entity.cart.CartItem;
 import entity.media.Media;
-
+// ISP: BaseController chứa các phương thức các lớp con không sử dụng đến 
 /**
  * This class is the base controller for our AIMS project
  * @author nguyenlm
